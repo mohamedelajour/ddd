4b2a2c0119a5657f19d2946fd134583104bb467a
mohamedelajour <mohamedelajour@gmail.com>
2024-08-14 16:41:36 +0300

    Add files via upload

---
+++ java/main.js
@@ -0,0 +1,9 @@
+document.getElementById("copy").onclick=function(){
+   alert(" تم نسخ النص");
+}
+document.getElementById("cop").onclick=function(){
+   alert(" تم نسخ النص");
+}
+document.getElementById("co").onclick=function(){
+   alert(" تم نسخ النص");
+}
